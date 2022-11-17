2ac73a715ef5674df3722614b74bf4eeca923379
muhammadisa <msawjk@gmail.com>
2022-11-18 03:01:23 +0700

    add unit test coverage

--- stream_test.go
+++ stream_test.go
@@ -1,1 +1,31 @@
 package oni
+
+import (
+	"github.com/segmentio/kafka-go"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type TestStreamSuite struct {
+	suite.Suite
+}
+
+func TestStreamTestSuite(t *testing.T) {
+	suite.Run(t, new(TestStreamSuite))
+}
+
+func (suite *ContextTestSuite) TestCloseProducersStream() {
+	suite.Run("TestCloseProducersStream", func() {
+		s := NewStream(kafka.ReaderConfig{
+			Brokers: []string{"localhost:8097"},
+			Topic:   "test",
+			GroupID: "consumer-group-test",
+		})
+
+		suite.Assert().Nil(s.closeProducers())
+		suite.Assert().Nil(s.producers["a"])
+		suite.Assert().Nil(s.producers["b"])
+		suite.Assert().Nil(s.producers["c"])
+		suite.Assert().Len(s.producers, 0)
+	})
+}
